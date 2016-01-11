f14ff9217e587626fc4cffd81af6005e8ccdc89a
puritys <puritys.chen@outlook.com>
2016-01-12 00:01:44 +0800

    add unit test

--- tests/unit/lib.js
+++ tests/unit/lib.js
@@ -30,11 +30,51 @@ describe("Test parseUrl", function () {
         url = "http://localhost:8080/";
         ret = obj.parseUrl(url);
         assert.equal(8080, ret.port);
+    });
+});
 
-
+describe("Test stringify parameter", function () {
+    it("parameter with normal type", function () {
+        var params, ret;
+        params = {
+            "str": "aaa",
+            "int": 123,
+            "bool": true,
+            "bool2": false
+        };
+        ret = obj.stringifyParam(params);
+        assert.equal('str=aaa&int=123&bool=true&bool2=false', ret);
     });
 
+    it("parameter with array", function () {
+        var params, ret;
+        params = {
+            "ay": [1,2,3]
+        };
+        ret = obj.stringifyParam(params);
+        assert.equal('ay[0]=1&ay[1]=2&ay[2]=3', ret);
+    });
 
+    it("parameter with object", function () {
+        var params, ret;
+        params = {
+            "obj": {
+                "a": "b",
+                "c":"d"
+            }
+        };
+        ret = obj.stringifyParam(params);
+        assert.equal('obj[a]=b&obj[c]=d', ret);
+    });
+
+    it("parameter with object inside array", function () {
+        var params, ret;
+        params = {
+            "ay": [1,{"a":"b"}]
+        };
+        ret = obj.stringifyParam(params);
+        assert.equal('ay[0]=1&ay[1][a]=b', ret);
+    });
 
 
 });
